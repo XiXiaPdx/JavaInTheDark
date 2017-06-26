6d58115f5de71adce82230a1563a800f9026cf06
Xi Xia <triplextension@gmail.com>
2017-06-26 08:51:04 -0700

    speech to text recognition is on

--- app/src/main/java/com/blueoxgym/javainthedark/Fragments/LevelTwo.java
+++ app/src/main/java/com/blueoxgym/javainthedark/Fragments/LevelTwo.java
@@ -1,18 +1,40 @@
 package com.blueoxgym.javainthedark.Fragments;
 
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.blueoxgym.javainthedark.R;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LevelTwo extends Fragment {
+public class LevelTwo extends Fragment implements View.OnClickListener {
+    @BindView(R.id.txt_output)TextView txtOutput;
+    @BindView(R.id.btn_mic)
+    ImageButton btnMicrophone;
+    private final int SPEECH_RECOGNITION_CODE = 1;
+
+
 
 
     public LevelTwo() {
@@ -24,7 +46,10 @@ public class LevelTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_level_two, container, false);
+        View view = inflater.inflate(R.layout.fragment_level_two, container, false);
+        ButterKnife.bind(this, view);
+        btnMicrophone.setOnClickListener(this);
+        return view;
     }
 
     public static LevelTwo newInstance(){
@@ -32,4 +57,40 @@ public class LevelTwo extends Fragment {
         return levelTwoFragment;
     }
 
+    @Override
+    public void onClick(View v) {
+        if (v == btnMicrophone){
+            startSpeechToText();
+        }
+
+    }
+
+    public void startSpeechToText(){
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start Speaking...");
+        try {
+            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getActivity(),
+                    "Sorry! Speech recognition is not supported in this device.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case SPEECH_RECOGNITION_CODE: {
+                if (resultCode == RESULT_OK && null != data) {
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    String text = result.get(0);
+                    txtOutput.setText(text);
+                }
+                break;
+            }
+        }
+    }
 }
