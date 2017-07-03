39aea10d675f1ea015a24c7b6a3678aaffbebb7c
Xi Xia <triplextension@gmail.com>
2017-07-03 11:45:53 -0700

    second level coming into shape

--- app/src/main/java/com/blueoxgym/javainthedark/Fragments/LevelTwo.java
+++ app/src/main/java/com/blueoxgym/javainthedark/Fragments/LevelTwo.java
@@ -37,10 +37,13 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
     @BindView(R.id.txt_output)TextView txtOutput;
     @BindView(R.id.btn_mic) ImageButton btnMicrophone;
     @BindView(R.id.lyricTextView) TextView lyricText;
+    @BindView(R.id.levelTextView) TextView levelText;
     private final int SPEECH_RECOGNITION_CODE = 1;
     private final String lyric = "Jessica's, This Way.";
     private String verseNoPunc;
-
+    private int currentLevel = 1;
+//    private String[] referenceWords;
+    private ArrayList displayWords;
 
     public LevelTwo() {
         // Required empty public constructor
@@ -54,8 +57,12 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
         View view = inflater.inflate(R.layout.fragment_level_two, container, false);
         ButterKnife.bind(this, view);
         btnMicrophone.setOnClickListener(this);
-        removePunc(lyric);
+        setVerseNoPunc(lyric);
         lyricText.setText(lyric);
+        levelText.setText("LEVEL "+ currentLevel);
+        displayWords = new ArrayList<String>();
+        startLevelTwo();
+
         return view;
     }
 
@@ -94,11 +101,7 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
                     ArrayList<String> result = data
                             .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
                     String text = result.get(0).toLowerCase();
-                    if(text.equals(verseNoPunc)) {
-                        txtOutput.setText(text);
-                    } else {
-                        txtOutput.setText("Not Quite..." + text);
-                    }
+                    checkForMatch(text);
 
                 }
                 break;
@@ -106,8 +109,78 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
         }
     }
 
-    public void removePunc(String lyric){
+    public void checkForMatch(String speech){
+        if (currentLevel == 1){
+           if(isInstaMatch(speech)){
+               //advance to Level 2
+               startLevelTwo();
+           } else {
+               txtOutput.setText("Please try again..." + speech);
+           }
+        } else if (currentLevel == 2){
+            if (isInstaMatch(speech)){
+                lyricText.setText(lyric);
+            } else {
+                String[] speechWords = speech.split(" ");
+                Log.d("Speech words", TextUtils.join(" ", speechWords));
+            }
+        }
+    }
+
+    public Boolean isInstaMatch(String speech){
+        if(speech.equals(verseNoPunc)) {
+            txtOutput.setText(speech);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void startLevelTwo() {
+        currentLevel = 2;
+        levelText.setText("LEVEL " + 2);
+        showFirstLetter();
+
+    }
+
+
+    public void showFirstLetter(){
+        String[] referenceWords = lyric.split(" ");
+        //make Display array of words
+        for ( int i=0; i < referenceWords.length; i++){
+            //current word in the loop
+            String tempWord = referenceWords[i];
+            //if word is length 1, then only "-"
+            if (tempWord.length() == 1){
+                displayWords.add("-");
+                // corner case   "I,"  will create issues
+            } else {
+                //loop over it and create new word character by character
+                String newDisplayWord="";
+                for (int j=0; j < tempWord.length(); j++){
+                    // display first letter of word
+                    if (j==0){
+                        newDisplayWord = newDisplayWord + tempWord.charAt(j);
+                    } else {
+                        // add "-"
+                        if (String.valueOf(tempWord.charAt(j)).matches("[a-zA-Z]")){
+                            newDisplayWord = newDisplayWord +"-";
+                        } else {
+                            // keep punctuation
+                            newDisplayWord = newDisplayWord + tempWord.charAt(j);
+                        }
+                    }
+                }
+                displayWords.add(newDisplayWord);
+            }
+        }
+        lyricText.setText(TextUtils.join(" ", displayWords));
+    }
+
+    public void setVerseNoPunc(String lyric){
         String [] words = lyric.replaceAll("[^a-zA-Z' ]", "").toLowerCase().split("\\s+");
         verseNoPunc = TextUtils.join(" ", words);
     }
+
+
 }
