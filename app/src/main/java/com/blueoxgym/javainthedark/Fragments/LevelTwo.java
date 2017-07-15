f800133169ecd4b921fd042dd101a696cec007f5
Xi Xia <triplextension@gmail.com>
2017-07-15 11:19:09 -0700

    non dialog microphone works with volume indictator

--- app/src/main/java/com/blueoxgym/javainthedark/Fragments/LevelTwo.java
+++ app/src/main/java/com/blueoxgym/javainthedark/Fragments/LevelTwo.java
@@ -3,10 +3,12 @@ package com.blueoxgym.javainthedark.Fragments;
 
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.res.ColorStateList;
 import android.media.Image;
 import android.os.Bundle;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
 import android.support.v4.app.Fragment;
 import android.text.TextUtils;
 import android.util.Log;
@@ -14,10 +16,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.blueoxgym.javainthedark.MainActivity;
 import com.blueoxgym.javainthedark.R;
 
 import org.w3c.dom.Text;
@@ -39,19 +44,26 @@ import static android.app.Activity.RESULT_OK;
  * A simple {@link Fragment} subclass.
  */
 public class LevelTwo extends Fragment implements View.OnClickListener {
-    @BindView(R.id.txt_output)TextView txtOutput;
-    @BindView(R.id.btn_mic) ImageButton btnMicrophone;
-    @BindView(R.id.lyricTextView) TextView lyricText;
-    @BindView(R.id.levelTextView) TextView levelText;
+    @BindView(R.id.txt_output)
+    TextView txtOutput;
+    @BindView(R.id.btn_mic)
+    ImageButton btnMicrophone;
+    @BindView(R.id.lyricTextView)
+    TextView lyricText;
+    @BindView(R.id.levelTextView)
+    TextView levelText;
+    @BindView(R.id.progressBarMic)
+    ProgressBar micLevels;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
     private final int SPEECH_RECOGNITION_CODE = 1;
     private String lyric;
-
-
     private String verseNoPunc;
     private int currentLevel = 1;
     private String[] referenceWords;
     private ArrayList displayWords;
     private ArrayList previousDisplayWords;
+    private final String TAG = "Voice Recognition";
 
     public LevelTwo() {
         // Required empty public constructor
@@ -68,12 +80,12 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
         lyric = getArguments().getString("oneVerse", "");
         setVerseNoPunc(lyric);
         lyricText.setText(lyric);
-        levelText.setText("LEVEL "+ currentLevel);
+        levelText.setText("LEVEL " + currentLevel);
         startLevel();
         return view;
     }
 
-    public static LevelTwo newInstance(String verse){
+    public static LevelTwo newInstance(String verse) {
         LevelTwo levelTwoFragment = new LevelTwo();
         Bundle args = new Bundle();
         args.putString("oneVerse", verse);
@@ -83,56 +95,56 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        if (v == btnMicrophone){
+        if (v == btnMicrophone) {
             startSpeechToText();
         }
 
     }
 
-    public void startSpeechToText(){
-        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start Speaking...");
-        try {
-            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
-        } catch (ActivityNotFoundException a) {
-            Toast.makeText(getActivity(),
-                    "Sorry! Speech recognition is not supported in this device.",
-                    Toast.LENGTH_SHORT).show();
-        }
-    }
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case SPEECH_RECOGNITION_CODE: {
-                if (resultCode == RESULT_OK && null != data) {
-                    ArrayList<String> result = data
-                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-                    String text = result.get(0).toLowerCase();
-                    if (isInstaMatch(text)) {
-                        currentLevel += 1;
-                        startLevel();
-                    } else {
-                        checkForMatch(text);
-                    }
-                    break;
-                }
-            }
+//    public void startSpeechToText(){
+//        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+//        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Start Speaking...");
+//        try {
+//            startActivityForResult(intent, SPEECH_RECOGNITION_CODE);
+//        } catch (ActivityNotFoundException a) {
+//            Toast.makeText(getActivity(),
+//                    "Sorry! Speech recognition is not supported in this device.",
+//                    Toast.LENGTH_SHORT).show();
+//        }
+//    }
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode) {
+//            case SPEECH_RECOGNITION_CODE: {
+//                if (resultCode == RESULT_OK && null != data) {
+//                    ArrayList<String> result = data
+//                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+//                    String text = result.get(0).toLowerCase();
+//                    if (isInstaMatch(text)) {
+//                        currentLevel += 1;
+//                        startLevel();
+//                    } else {
+//                        checkForMatch(text);
+//                    }
+//                    break;
+//                }
+//            }
+//        }
+//    }
+
+    public void checkForMatch(String speech) {
+        if (currentLevel == 1) {
+            txtOutput.setText("Please try again..." + speech);
+        } else {
+            checkEachWord(speech);
         }
     }
 
-    public void checkForMatch(String speech){
-       if (currentLevel == 1) {
-           txtOutput.setText("Please try again..." + speech);
-       } else {
-                checkEachWord(speech);
-        }
-    }
-
-    public Boolean isInstaMatch(String speech){
-        if(speech.toLowerCase().equals(verseNoPunc)) {
+    public Boolean isInstaMatch(String speech) {
+        if (speech.toLowerCase().equals(verseNoPunc)) {
             txtOutput.setText("You got it! ");
             return true;
         } else {
@@ -140,11 +152,11 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
         }
     }
 
-    public void checkEachWord (String speech){
+    public void checkEachWord(String speech) {
         String[] speechWords = speech.split(" ");
         txtOutput.setText("Close! Please Try Again");
         //reset displayWord
-        displayWords = new ArrayList <String>();
+        displayWords = new ArrayList<String>();
         //compare each word in speech array with reference array
         for (int i = 0; i < referenceWords.length; i++) {
             //speaker can say too few words. Check to prevent indexOutofBounds
@@ -170,65 +182,65 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
         setLyricTextView();
     }
 
-    public void revealMoreLetters(int i){
+    public void revealMoreLetters(int i) {
         // We know speech did not equal reference
         // use previousDisplayWords to know where to begin inserting new letter
         String previousWord = previousDisplayWords.get(i).toString();
-        if (alreadySolved(previousWord)){
+        if (alreadySolved(previousWord)) {
             setHintWords(previousWord);
             return;
         }
-            StringBuilder tempWord = new StringBuilder(previousWord);
-        int dashCount=0;
-        for (int k = 0; k < previousWord.length(); k++){
-            if (String.valueOf(previousWord.charAt(k)).equals("-")){
+        StringBuilder tempWord = new StringBuilder(previousWord);
+        int dashCount = 0;
+        for (int k = 0; k < previousWord.length(); k++) {
+            if (String.valueOf(previousWord.charAt(k)).equals("-")) {
                 dashCount++;
             }
         }
-            switch (currentLevel) {
-                case 2:
-                    if (dashCount > 1) {
-                        for (int j = 0; j < previousWord.length(); j++) {
-                            if (String.valueOf(previousWord.charAt(j)).equals("-")) {
-                                tempWord.setCharAt(j, referenceWords[i].charAt(j));
-                                displayWords.add(tempWord);
-                                break;
-                            }
+        switch (currentLevel) {
+            case 2:
+                if (dashCount > 1) {
+                    for (int j = 0; j < previousWord.length(); j++) {
+                        if (String.valueOf(previousWord.charAt(j)).equals("-")) {
+                            tempWord.setCharAt(j, referenceWords[i].charAt(j));
+                            displayWords.add(tempWord);
+                            break;
                         }
-                    }else {
-                        displayWords.add(previousWord);
                     }
-                    break;
-                case 3:
-                case 5:
+                } else {
                     displayWords.add(previousWord);
-                    break;
-                case 4:
-                    if (dashCount > 1) {
-                        Random ran = new Random();
-                        int randomIndex = 0;
-                        Character randomLetter;
-                        do {
-                            randomIndex = ran.nextInt(referenceWords[i].length());
-                            randomLetter = referenceWords[i].charAt(randomIndex);
-                        }
-                        while (randomIndex == 0 || ifEndInPunc(randomLetter) || isAtoZ(tempWord.charAt(randomIndex)));
-                        tempWord.setCharAt(randomIndex, randomLetter);
-                        displayWords.add(tempWord);
-                    } else {
-                        displayWords.add(previousWord);
+                }
+                break;
+            case 3:
+            case 5:
+                displayWords.add(previousWord);
+                break;
+            case 4:
+                if (dashCount > 1) {
+                    Random ran = new Random();
+                    int randomIndex = 0;
+                    Character randomLetter;
+                    do {
+                        randomIndex = ran.nextInt(referenceWords[i].length());
+                        randomLetter = referenceWords[i].charAt(randomIndex);
                     }
-                    break;
+                    while (randomIndex == 0 || ifEndInPunc(randomLetter) || isAtoZ(tempWord.charAt(randomIndex)));
+                    tempWord.setCharAt(randomIndex, randomLetter);
+                    displayWords.add(tempWord);
+                } else {
+                    displayWords.add(previousWord);
+                }
+                break;
 
         }
     }
 
     public void startLevel() {
-        displayWords= new ArrayList<String>();
+        displayWords = new ArrayList<String>();
         levelText.setText("LEVEL " + currentLevel);
         //make Display array of lyric words
         referenceWords = lyric.split(" ");
-        switch (currentLevel){
+        switch (currentLevel) {
             case 1:
                 displayWords = new ArrayList<String>(Arrays.asList(referenceWords));
                 break;
@@ -236,7 +248,7 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
             case 3:
             case 4:
             case 5:
-                for ( int i=0; i < referenceWords.length; i++) {
+                for (int i = 0; i < referenceWords.length; i++) {
                     String tempWord = referenceWords[i];
                     setHintWords(tempWord);
                 }
@@ -284,7 +296,7 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
                                 newDisplayWord = newDisplayWord + "-";
                                 break;
                         }
-                    } else if ( j == (tempWord.length()-1) && ifEndInPunc(tempWord.charAt(j))){
+                    } else if (j == (tempWord.length() - 1) && ifEndInPunc(tempWord.charAt(j))) {
                         newDisplayWord = newDisplayWord + tempWord.charAt(j);
                     }
                 }
@@ -292,56 +304,133 @@ public class LevelTwo extends Fragment implements View.OnClickListener {
                 break;
         }
     }
-    
 
-public Boolean alreadySolved(String word){
-    if (word.contains("-")){
-        return false;
-    } else {
-        return true;
+
+    public Boolean alreadySolved(String word) {
+        if (word.contains("-")) {
+            return false;
+        } else {
+            return true;
+        }
     }
-}
 
-public Boolean isAtoZ(char letter){
-    if(String.valueOf(letter).matches("[a-zA-Z]")){
-        return true;
-    }
-    return false;
-}
-
-public Boolean isOneLetterWord(String tempWord){
-    if (tempWord.length() == 1){
-        displayWords.add("-");
-        return true;
-    } else if (tempWord.length() == 2 && ifEndInPunc(tempWord.charAt(1))) {
-        // corner case   "I,"
-        displayWords.add("-" + tempWord.charAt(1));
-        return true;
-    } else{
+    public Boolean isAtoZ(char letter) {
+        if (String.valueOf(letter).matches("[a-zA-Z]")) {
+            return true;
+        }
         return false;
     }
-}
 
-    public void setVerseNoPunc(String lyric){
-        String [] words = lyric.replaceAll("[^a-zA-Z' ]", "").toLowerCase().split("\\s+");
+    public Boolean isOneLetterWord(String tempWord) {
+        if (tempWord.length() == 1) {
+            displayWords.add("-");
+            return true;
+        } else if (tempWord.length() == 2 && ifEndInPunc(tempWord.charAt(1))) {
+            // corner case   "I,"
+            displayWords.add("-" + tempWord.charAt(1));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setVerseNoPunc(String lyric) {
+        String[] words = lyric.replaceAll("[^a-zA-Z' ]", "").toLowerCase().split("\\s+");
         verseNoPunc = TextUtils.join(" ", words);
     }
 
-    public String removeWordPunc(String word){
+    public String removeWordPunc(String word) {
         return word.replaceAll("[^a-zA-Z' ]", "").toLowerCase();
     }
 
-    public Boolean ifEndInPunc(Character letter){
+    public Boolean ifEndInPunc(Character letter) {
         Pattern p = Pattern.compile("[,.?!:']");
         Matcher m = p.matcher(String.valueOf(letter));
-        if(m.find()){
+        if (m.find()) {
             return true;
         }
-    return false;
+        return false;
     }
 
-    public void setLyricTextView (){
+    public void setLyricTextView() {
         previousDisplayWords = displayWords;
         lyricText.setText(TextUtils.join(" ", displayWords));
     }
+
+    class listener implements RecognitionListener {
+        @Override
+        public void onReadyForSpeech(Bundle params) {
+            Log.d(TAG, "onReadyForSpeech");
+        }
+
+        @Override
+        public void onBeginningOfSpeech() {
+            Log.d(TAG, "onBeginningOfSpeech");
+            micLevels.setMax(10);
+        }
+
+        @Override
+        public void onRmsChanged(float rmsdB) {
+            Log.d(TAG, String.valueOf(rmsdB));
+            micLevels.setProgress((int) rmsdB);
+        }
+
+        @Override
+        public void onBufferReceived(byte[] buffer) {
+            Log.d(TAG, "onBufferReceived");
+
+        }
+
+        @Override
+        public void onEndOfSpeech() {
+            Log.d(TAG, "onEndofSpeech");
+            speech.stopListening();
+        }
+
+        @Override
+        public void onError(int error) {
+            Log.d(TAG, "error " + error);
+        }
+
+        @Override
+        public void onResults(Bundle results) {
+            String str = new String();
+            Log.d(TAG, "onResults " + results);
+//
+//            ArrayList<String> result = results.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+
+            ArrayList<String> data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+            String text = data.get(0).toLowerCase();
+            if (isInstaMatch(text)) {
+                currentLevel += 1;
+                startLevel();
+            } else {
+                checkForMatch(text);
+            }
+        }
+
+    @Override
+    public void onPartialResults(Bundle partialResults) {
+        Log.d(TAG, "onPartialResults");
+
+    }
+
+    @Override
+    public void onEvent(int eventType, Bundle what) {
+        Log.d(TAG, "onEvent " + eventType);
+    }
+
+}
+    public void startSpeechToText(){
+        speech=SpeechRecognizer.createSpeechRecognizer(getContext());
+        speech.setRecognitionListener(new listener());
+        recognizerIntent= new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE, "en-US");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getActivity().getPackageName());
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
+        speech.startListening(recognizerIntent);
+    }
+
 }
